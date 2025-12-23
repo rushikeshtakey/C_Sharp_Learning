0adbfddd8ff21dbce5d27f9abd7dfc34403de5f3
rushikeshtakey <your_correct_email@example.com>
2025-12-23 10:42:55 +0530

    Learned Write() v/s WriteLine, Comments, Formate Specifiers and Escape Sequence

---
+++ Output/Output/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This is a Write Method ");
+            Console.WriteLine("This is a WriteLine method");
+
+            //this is a single line comment
+            /*
+             this is a
+             multiline
+             comment
+             */
+
+            //using escape sequence
+
+            Console.WriteLine("Formate specifier for (\\t)Tab->\t<-");
+            Console.WriteLine("Formate specifier for (\\n)New line->\n<-");
+            Console.WriteLine("Formate specifier for (\\b)Backspace->\b<-");
+            Console.WriteLine("Formate specifier for (\\a)Bell alert sound->\a<-");
+            Console.WriteLine("Formate specifier for (\\f) forward feed->\f<-");
+            Console.WriteLine("Formate specifier for (\\r) carrage return->\r<-");
+            Console.WriteLine("Formate specifier for (\\v) vertical tab->\v<-");
+            Console.WriteLine("Escape sequence for back slash is backslash = \\\\");
+            Console.WriteLine("Escape sequence for Double and single Quotes is backslash = \" Double and Single Quotes \'");
+
+            Console.ReadLine();
+
+        }
+    }
+}
