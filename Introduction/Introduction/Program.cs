d723387432dff18fff3e31483984c20164bdadf6
rushikeshtakey <your_correct_email@example.com>
2025-12-23 09:47:25 +0530

    Hello world C# program with Beep sound.

---
+++ Introduction/Introduction/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Introduction
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+        Console.WriteLine("Hello, World!");
+            Console.WriteLine("I am a C# Programmer");
+            Console.Beep();
+    
+        }
+    }
+}
