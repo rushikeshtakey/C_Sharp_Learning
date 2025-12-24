a7928aa1da0284b1fed9f88666dc75a6fcd7e644
rushikeshtakey <your_correct_email@example.com>
2025-12-24 14:56:06 +0530

    Learned some string methods

---
+++ Day-2/StringMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StringMethod
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Clear();
+			String name = "Rushikesh Takey";
+			
+			Console.WriteLine(name.ToUpper());
+			Console.WriteLine(name.ToLower());
+
+			String phoneNumber = "9595-404-211";
+			Console.WriteLine(phoneNumber.Replace("-",""));
+
+			Console.WriteLine(name.Insert(0,"Mr. "));
+			Console.WriteLine("First Name: " + name.Substring(0,10));
+			Console.WriteLine("Last Name: " + name.Substring(10,5));
+
+			Console.WriteLine("Length of Name: " + name.Length);
+			
+			Console.ReadKey();
+		}
+	}
+}
+
+
+
