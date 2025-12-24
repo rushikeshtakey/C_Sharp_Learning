a94fad8ed6782e3c4343147aac2300be3cf17a6e
rushikeshtakey <your_correct_email@example.com>
2025-12-24 12:47:41 +0530

    Learned how to generate random numbers in C#

---
+++ Day-2/Math_Class/Random_number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Random_nummber
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Clear();
+
+			Random random = new Random();
+			int rn = random.Next(1,7);
+
+			Console.WriteLine("Random number between 1 and 7 is: " + rn);
+			
+			double drn = random.NextDouble();
+			Console.WriteLine("Random decimal number between 0 and 1 is:" + drn);
+
+			Console.ReadKey();
+		}
+	}
+}
