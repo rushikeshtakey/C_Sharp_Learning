8c6555b90c23de8282cd9cc24df849e254f71780
rushikeshtakey <rushikeshtakey@gmail.com>
2025-12-24 14:11:47 +0530

    Made hypotenus calculator

---
+++ Day-2/Hypotenuse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hypotenuse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Clear();
+			
+			Console.Write("Enter the value of side A: ");
+			double A = Convert.ToInt32(Console.ReadLine());
+			
+			Console.Write("Enter the value of side B: ");
+			double B = Convert.ToInt32(Console.ReadLine());
+
+			Double H = Math.Sqrt(((A*A)+(B*B)));
+			Console.Write("Hypotenuse is: " + H);			
+			Console.ReadKey();
+		}
+	}
+}
