796d04ebf5d1fce93acae9ae4016858a6812f1f7
rushikeshtakey <your_correct_email@example.com>
2025-12-24 15:43:31 +0530

    Learned if-else and else if Statements in C#

---
+++ Day-2/IfElseStatements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IfElseStatement
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Clear();
+
+			Console.WriteLine("What is your age: ");
+			int age = Convert.ToInt32(Console.ReadLine());
+
+			if(age<=0)
+			{
+				Console.WriteLine("First Learn to Walk");	
+			}
+			else if(age>=18)
+			{
+				Console.WriteLine("Can Vote");
+			}
+			else
+			{
+				Console.WriteLine("Can't Vote");
+			}
+			
+			Console.ReadKey();
+		}
+	}
+}
