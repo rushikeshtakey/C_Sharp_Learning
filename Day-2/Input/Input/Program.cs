969d40f6371f9611a42c97d06858bce0048f345f
rushikeshtakey <your_correct_email@example.com>
2025-12-24 10:45:13 +0530

    Made age caculator in Input.cs and made day wise file structure

---
+++ Day-2/Input/Input/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name:");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("What is your birth Year:");
+            int birYar = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+
+            Console.WriteLine(name + " Your age is: " +  (2025 - birYar) + "years");
+
+            Console.ReadKey();
+
+        }
+    }
+}
