0bf2b3756b9f85aa0a90d1f13586cf1167461c99
rushikeshtakey <your_correct_email@example.com>
2025-12-25 14:32:12 +0530

    made Day 3 Directory and mad number guessing game

---
+++ Day-3/NumberGuessingGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Clear();
+			char play = 'y';
+			int num = 0;
+			int guess = 0;
+			Random random = new Random();
+
+			do
+			{
+				num = random.Next(1,101);
+
+				do
+				{
+					Console.WriteLine("Guess the number between 1-100");
+					guess = Convert.ToInt32(Console.ReadLine());
+
+					if (guess < num)
+					{
+						Console.WriteLine("Guess Greater");
+					}
+					else
+					{
+						Console.WriteLine("Guess Smaller");
+					}
+	
+				}while(num != guess);
+
+				Console.WriteLine(num+" is correct guess");
+				
+				Console.WriteLine("Do you want to play again:(y/n)");
+				play = Convert.ToChar(Console.ReadLine());
+	
+
+			}while(play == 'y');
+
+			Console.ReadKey();	
+
+
+
+		}
+	}
+}
+
