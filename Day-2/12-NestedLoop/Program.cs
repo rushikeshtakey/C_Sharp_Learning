c17dc5ac1b9f1cc97431dae6a2827c242ee956e3
rushikeshtakey <your_correct_email@example.com>
2025-12-25 11:33:47 +0530

    Program to make holo rectrangel with nested for loop

---
+++ Day-2/12-NestedLoop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NestedLoop
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Clear();
+
+			Console.WriteLine("Enter height: ");
+			int h = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Enter width: ");
+			int w = Convert.ToInt32(Console.ReadLine()); 
+			Console.WriteLine("Enter symbol: ");
+			char s = Convert.ToChar(Console.ReadLine());
+
+			for(int i = 0; i < h; i++)
+			{
+				for(int j = 0; j < w; j++)
+				{
+					if(i==0 || i== h-1 || j==0 || j == w-1)
+					{
+						Console.Write(s);
+					}
+					else
+					{
+						Console.Write(" ");
+					}
+				}
+				Console.WriteLine();
+			}
+			
+			
+
+			Console.ReadKey();
+		}
+	}
+}
+
