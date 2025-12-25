bde518b12936ceb1571fd0c2767fef270d113f58
rushikeshtakey <your_correct_email@example.com>
2025-12-25 10:19:22 +0530

    Handeling empty names submition by while and do-while loops

---
+++ Day-2/10-WhileAndDoWhileLoops/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WhileLoops
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Clear();
+
+			//While Loop Implimentation:
+
+			String fname = "";
+
+			while(fname == "")
+			{
+				Console.WriteLine("What is your first name:");
+				fname = Console.ReadLine();
+			}
+
+			Console.Clear();
+
+			//Do-While loop  implimentation:
+
+			String lname = "";
+
+			do
+			{
+				Console.WriteLine("What is your Last Name: ");
+				lname = Console.ReadLine();
+				
+			}while(lname == "");
+
+			Console.Clear();
+
+			Console.WriteLine("Your Name is: " + fname + " " + lname);
+
+			Console.ReadKey();
+		}
+	}
+}
+
+
