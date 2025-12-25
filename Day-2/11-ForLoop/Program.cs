f16341e93ac5643cd3b2eb8f138351ad6d8f6460
rushikeshtakey <your_correct_email@example.com>
2025-12-25 11:03:53 +0530

    printed odd numbers from 3 to given input via for loop

---
+++ Day-2/11-ForLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ForLoop
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			//print odd numbers from 3 to given input
+			Console.Clear();
+
+			Console.WriteLine("Enter a number greater than 3: ");
+			int num = Convert.ToInt32(Console.ReadLine());
+
+			for(int i = 3; i<= num; i+=2)
+			{
+				Console.WriteLine(i);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
+
