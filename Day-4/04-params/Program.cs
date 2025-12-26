380dae6773045cea83196fc5adb93e9aed8a5a7c
rushikeshtakey <your_correct_email@example.com>
2025-12-26 10:49:02 +0530

    Learned how to use params keyword

---
+++ Day-4/04-params/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Params
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Clear();
+
+			Mult(1,2,3);
+			Mult(1,2,3,4,5);
+			Mult(1,2,3,4,5,6);
+
+			Console.ReadKey();
+		}
+
+		static void Mult(params int[] numbers)
+		{
+			int ans = 1;
+			foreach(int number in numbers)
+			{
+				ans *= number;
+			}
+
+			Console.Write("Multiplication of ");
+
+			foreach(int number in numbers)
+			{
+				Console.Write(number + " ");
+			}
+
+			Console.WriteLine("is " + ans);
+
+		}
+	}
+}
