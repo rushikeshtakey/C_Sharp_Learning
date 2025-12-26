42daf963979bdb8fcc3203aabc211b5250755650
rushikeshtakey <your_correct_email@example.com>
2025-12-26 13:45:48 +0530

    Learned Multidimentional Array

---
+++ Day-5/03-MultidimentionalArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MultidimentionalArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[,] Tables = {	
+						{2,4,6,8,10},
+						{3,6,9,12,14},
+						{4,8,12,17,20}
+					   };
+
+			Tables[1,4] = 15;
+			Tables[2,3] = 16;
+
+			Console.WriteLine("Output from foreach loop:");
+			foreach(int table in Tables)
+			{
+				Console.Write(table + " ");
+			} 
+
+			Console.WriteLine("\nOutput from for loop:");
+
+			for(int i =0; i< Tables.GetLength(0); i++)
+			{
+				for(int j = 0; j<Tables.GetLength(1); j++)
+				{
+					Console.Write(Tables[i,j] + " ");
+				}
+				Console.WriteLine();
+			}
+			
+		}
+	}
+}
+
