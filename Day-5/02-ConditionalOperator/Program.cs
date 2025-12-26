84041bc250c56ebaeada2ddc61b8f82c57633c26
rushikeshtakey <your_correct_email@example.com>
2025-12-26 12:07:15 +0530

    Used conditional operator and string interpoletion

---
+++ Day-5/02-ConditionalOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConditionalOperator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter Two Numbers:");
+			int n1 =  Convert.ToInt32(Console.ReadLine());
+			int n2 =  Convert.ToInt32(Console.ReadLine());
+			
+			Console.WriteLine($"Largest number between {n1} and {n2} is : {Max(n1,n2)}");
+		}
+
+		static int Max(int n1, int n2)
+		{
+			return ((n1>n2) ? n1 : n2);
+		}
+	}
+}
+
