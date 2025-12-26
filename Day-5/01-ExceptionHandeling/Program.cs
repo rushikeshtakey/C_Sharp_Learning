c8c7b80c2fc9d89c3aa7cd48f02f21dccbe73a5a
rushikeshtakey <your_correct_email@example.com>
2025-12-26 11:26:18 +0530

    Started day 5 and learned basic exception handeling

---
+++ Day-5/01-ExceptionHandeling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExceptionHandeling
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Clear();
+			
+			int x;
+			int y;
+			
+			try
+			{
+				Console.WriteLine("Enter 2 numbers: ");
+				x = Convert.ToInt32(Console.ReadLine());
+				y = Convert.ToInt32(Console.ReadLine());
+			
+				Console.WriteLine("Division is: " + (x/y));
+			}
+			catch(DivideByZeroException e)
+			{
+				Console.WriteLine("Can't Devide by zero");
+			}
+			catch(FormatException e)
+			{
+				Console.WriteLine("Please enter a number");
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine("Something went wrong");
+			}
+			finally
+			{
+				Console.WriteLine("This is Finally block");
+			}
+
+			Console.WriteLine("Main ends");
+			Console.ReadKey();
+		}
+	}
+}
+
+
