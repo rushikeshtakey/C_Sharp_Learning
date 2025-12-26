efff5ed403400fb3040cfeb5b6fef50cba7fd841
rushikeshtakey <your_correct_email@example.com>
2025-12-26 09:54:51 +0530

    Learned to make methods and call them and return some value

---
+++ Day-4/02-Method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Method
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+				Console.Clear();
+
+				Console.WriteLine("Enter Two Numbers:(Each number seperated by enter)");
+				double n1 = Convert.ToDouble(Console.ReadLine());
+				double n2 = Convert.ToDouble(Console.ReadLine());
+				Console.WriteLine("Multiplication is: " + Multi(n1,n2));
+
+				Console.ReadKey();		
+		}
+
+		static double Multi(double n, double m)
+		{
+			return m*n;
+		}
+	}
+}
+
+
