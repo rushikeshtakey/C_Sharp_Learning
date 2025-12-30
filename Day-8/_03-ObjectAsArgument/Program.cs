06bf9449af7e23c55710ea6b59ebd25395de89c6
rushikeshtakey <rushikeshtakey@gmail.com>
2025-12-30 12:05:58 +0530

    Learned how to pass and return an object from a method

---
+++ Day-8/_03-ObjectAsArgument/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ObjPassing
+{
+    class Program
+    {
+        static void Main()
+        {
+            Laptop l1 = new Laptop("Asus",16);
+            Laptop l2 = Copy(l1);
+
+            Console.WriteLine(l2.Brand + " " + l2.ram + "GB ram");
+        }
+
+        static public Laptop Copy(Laptop lp)
+        {
+            return new Laptop(lp.Brand,lp.ram);
+        }
+    }
+
+
+    class Laptop
+    {
+        public String Brand;
+        public sbyte ram;
+        public Laptop(String Brand, sbyte ram) 
+        {
+            this.Brand = Brand;
+            this.ram = ram;
+        }
+    }
+}
