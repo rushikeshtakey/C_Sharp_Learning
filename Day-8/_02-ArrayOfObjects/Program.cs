453a9d03ee3d29159b2f6a386f93a360326efaab
rushikeshtakey <your_correct_email@example.com>
2025-12-30 11:14:49 +0530

    Learned how to declare and initialise the array of objects

---
+++ Day-8/_02-ArrayOfObjects/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ObjArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Declaration
+            Phone[] shop = new Phone[3];
+            //Objects
+            Phone p1 = new Phone("IPhone");
+            Phone p2 = new Phone("Samsung");
+            Phone p3 = new Phone("OnePlus");
+            //Initializetion/Assignment
+            shop[0] = p1;
+            shop[1] = p2;
+            shop[2] = p3;
+            foreach(Phone phone in shop)
+            {
+                Console.WriteLine(phone.Brand);
+            }
+            Console.WriteLine();
+            //Declaration+Assignment
+            Phone[] Chhshop = { new Phone("Nokiya"), new Phone("Micromax"), new Phone("Lava") };
+
+            foreach (Phone phone in Chhshop)
+            {
+                Console.WriteLine(phone.Brand);
+            }
+        }
+
+
+
+        
+    }
+
+    class Phone
+    {
+        public String Brand;
+        public Phone(String brand)
+        {
+            this.Brand = brand;
+        }
+    }
+}
