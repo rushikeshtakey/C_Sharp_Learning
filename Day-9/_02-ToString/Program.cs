0480d68927588102add90d2334a9bfd18fba771d
rushikeshtakey <your_correct_email@example.com>
2025-12-31 14:36:26 +0530

    Learned Method overriding and polymorphism on day 9

---
+++ Day-9/_02-ToString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Phone p1 = new Phone("MotoG fusion g40",8,"Snapdragon gen8",256);
+            Console.WriteLine(p1.ToString());
+            
+            Console.ReadKey();
+        }
+    }
+
+    class Phone
+    {
+        String brand;
+        int ram;
+        String Processor;
+        int storage;
+
+        public Phone(string brand, int ram, string processor, int storage )
+        {   
+            this.brand = brand;
+            this.ram = ram;
+            Processor = processor;
+            this.storage = storage;
+        }
+
+        public override string ToString()
+        {
+            return ($"This is {brand} of {ram}GB ram.");
+        }
+    }
+
+}
