a29f2dc0975fcd6256d8426bb334beb7ce6c8d76
rushikeshtakey <rushikeshtakey@gmail.com>
2025-12-31 14:36:26 +0530

    Learned Method overriding and polymorphism on day 9

---
+++ Day-9/_01-MethodOverriding/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MethodOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Animals animal = new Animals();
+            Dogs dog = new Dogs();
+            Cats cat = new Cats();
+
+            animal.Sound();
+            dog.Sound();
+            cat.Sound();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Animals
+    {
+
+        public virtual void Sound() 
+        {
+            Console.WriteLine("animel sound");
+        }
+
+    }
+
+    class Dogs : Animals
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("bhau-bhau");
+        }
+    }
+
+    class Cats : Animals 
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("meow-meow");
+        }
+
+    }
+}
---
+++ Day-9/_03-Polymorphism/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Polymorphisum
+{
+    class Program
+    {
+        static void Main()
+        {
+            Phone dp = new Phone("Moto G40 fusion",8,128);
+            Laptop dl = new Laptop("ASUS Vivobook",16,512);
+            SmartWatch dsw = new SmartWatch("Samsung",3,32);
+            DegitalDevice[] degitalDevices = { dp,dl,dsw};
+
+            foreach(DegitalDevice device in degitalDevices) 
+            {
+                device.RamNStorage();
+            }
+
+        }
+    }
+
+    class DegitalDevice
+    {
+
+        public virtual void RamNStorage()
+        { }
+
+    }
+
+    class Phone : DegitalDevice
+    {
+        String brand;
+        int ram, storage;
+
+        public Phone(String brand,int ram, int storage)
+        {
+            this.brand = brand;
+            this.ram = ram;
+            this.storage = storage;
+        }
+        public override void RamNStorage()
+        {
+            Console.WriteLine($"this {brand} has {ram}GB of ram and {storage}GB of storage.");
+        }
+    }
+
+    class Laptop : DegitalDevice
+    {
+        String brand;
+        int ram, storage;
+
+        public Laptop(String brand, int ram, int storage)
+        {
+            this.brand = brand;
+            this.ram = ram;
+            this.storage = storage;
+        }
+        public override void RamNStorage()
+        {
+            Console.WriteLine($"this {brand} has {ram}GB of ram and {storage}GB of storage.");
+        }
+    }
+
+    class SmartWatch : DegitalDevice
+    {
+        String brand;
+        int ram, storage;
+
+        public SmartWatch(String brand, int ram, int storage)
+        {
+            this.brand = brand;
+            this.ram = ram;
+            this.storage = storage;
+        }
+        public override void RamNStorage()
+        {
+            Console.WriteLine($"this {brand} has {ram}GB of ram and {storage}GB of storage.");
+        }
+    }
+
+}
