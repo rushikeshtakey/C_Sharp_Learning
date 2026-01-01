efd696573666ac8d93870ccf0decadeab7fdf75a
rushikeshtakey <rushikeshtakey@gmail.com>
2026-01-01 12:30:37 +0530

    On Day 10 I learned Interface and Lists in C#

---
+++ Day-10/_02-List/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> foods = new List<String>();
+            foods.Add("Dosa");
+            foods.Add("Samosa");
+            foods.Add("Kachori");
+            foods.Add("Kanda Bhaje");
+            foods.Add("Misal Pav");
+            foods.Add("Pav Bhaji");
+            foods.Add("Wada Pav");
+            foreach (String item in foods)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("foods.Remove(\"Dosa\")");
+            foods.Remove("Dosa");
+            foreach(String item in foods)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("foods.Insert(0, \"Wada Pav\")");
+            foods.Insert(0, "Wada Pav");
+            foreach (String item in foods)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(foods.Count());
+            Console.WriteLine(foods.IndexOf("Wada Pav"));
+            Console.WriteLine(foods.LastIndexOf("Wada Pav"));
+            Console.WriteLine(foods.Contains("Samosa"));
+            Console.WriteLine("foods.Sort()");
+            foods.Sort();
+            foreach (String item in foods)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("foods.Reverse()");
+            foods.Reverse();
+            foreach (String item in foods)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("foods.Clear()");
+            foods.Clear();
+            foreach (String item in foods)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Day-10/_03-ListOfObjects/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ListOfObjects
+{
+    class program
+    {
+        static void Main()
+        {
+            List<String> students = new List<String>();
+
+            char Continue = 'y';
+
+            do
+            {
+                Console.WriteLine("Enter Students name as per role number");
+                students.Add(new (Console.ReadLine()));
+                Console.WriteLine("Do you want to continue (y/n)");
+                Continue = Convert.ToChar(Console.ReadLine());
+            } while (Continue == 'y');
+
+            Console.WriteLine("total students are: " + students.Count());
+        }
+    }
+
+    class Student
+    {
+        String name;
+        public Student(String name)
+        {
+            this.name = name;
+        }
+    }
+}
