932ccfb965ba1ae8dd0448e773b8bb5de9ddffee
rushikeshtakey <rushikeshtakey@gmail.com>
2026-01-02 12:49:34 +0530

    Learned to make properties with getter and setters in C#

---
+++ Day-11/_01-GetNSet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GetNSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Speed = 2000;
+            Console.WriteLine("Speed of the car is " + car.Speed);
+        }
+    }
+
+    class Car
+    {
+        private int speed;
+        public Car()
+        {
+            Speed = speed;
+        }
+
+        public int Speed
+        {
+            get {  return speed; }
+            set
+            {
+                if (value > 500)
+                {
+                    speed = 500;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+        }
+    }
+}
