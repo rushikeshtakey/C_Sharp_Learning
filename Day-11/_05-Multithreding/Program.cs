a3773fe449679b3f9cd491b9aa46aef5129ddb59
rushikeshtakey <your_correct_email@example.com>
2026-01-03 17:52:16 +0530

    Tried basic multithreading in C#

---
+++ Day-11/_05-Multithreding/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Multithreding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            Thread thread1 = new Thread(p.CountUp);
+            Thread thread2 = new Thread(p.CountDown);
+            thread1.Start();
+            thread2.Start();
+
+        }
+
+        public void CountUp()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("This is Thread 1: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+        public void CountDown()
+        {
+            for(int i = 10; i >=0; i--)
+            {
+                Console.WriteLine("This is Thread 2: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+
+}
