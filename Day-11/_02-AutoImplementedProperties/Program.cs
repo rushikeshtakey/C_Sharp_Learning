28c4068ef89e24eccf8443a2269070732bbcfe21
rushikeshtakey <rushikeshtakey@gmail.com>
2026-01-03 17:00:24 +0530

    Learned getters and setters wit enums in C#

---
+++ Day-11/_02-AutoImplementedProperties/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AutoImplementedPropery
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Car car = new Car("Nano");
+            Console.WriteLine(car.Model);
+        }
+    }
+
+    class Car
+    {
+        public String Model { get; set; }
+        public Car(String model)
+        {
+            this.Model = model;
+        }
+    
+    }
+
+}
