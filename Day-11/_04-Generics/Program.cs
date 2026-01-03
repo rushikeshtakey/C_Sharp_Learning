bd405b17027dfd1a768b705ad0e2ee9fd19e13af
rushikeshtakey <your_correct_email@example.com>
2026-01-03 17:26:36 +0530

    Learned generic programming in C#

---
+++ Day-11/_04-Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3 };
+            double[] doubles = { 1.0, 2.0, 3.0 };
+            String[] strings = { "1", "2", "3" };
+
+            Show(ints);
+            Show(doubles);
+            Show(strings);
+        }
+
+        public static void Show<Thing>(Thing[] arr)
+        {
+            foreach(Thing thing in arr)
+            {
+                Console.Write(thing);
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+}
